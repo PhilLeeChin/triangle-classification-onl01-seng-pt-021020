26a0b379777207d39e344f28f4f7da467cbf3f4c
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-16 14:19:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,8 +11,12 @@ class Triangle
   def kind
     if (@side_a <= 0) || (@side_b <= 0) || (@side_c <= 0)
       raise TriangleError
+      rescue PartnerError => error
+        puts error.message
     elsif (@side_a + @side_b <= @side_c) || (@side_a + @side_c <= @side_b) || (@side_b + @side_c <= @side_a)
       raise TriangleError
+      rescue PartnerError => error
+        puts error.message
     else
       if((@side_a == @side_b) && (@side_b == @side_c))
         :equilateral
