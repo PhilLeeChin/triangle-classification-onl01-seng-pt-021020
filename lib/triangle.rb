0d7b9bc143d7fc241cdd52b16f5eafefbcf8afc3
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-16 14:10:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,9 @@ class Triangle
   end
 
   def kind
-    if((s_a == s_b) && (s_b == s_c))
+    if((@side_a == @side_b) && (@side_b == @side_c))
       :equilateral
-    elsif((s_a == s_b) || (s_b == s_c) || (s_c == s_a))
+    elsif((@side_a == @side_b) || (@side_b == @side_c) || (@side_c == @side_a))
       :isosceles
     else
       :scalene
